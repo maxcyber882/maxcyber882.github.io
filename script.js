f74739f3e5e3b93ad8501b9f8ef2dcee1a5db987
Maxcyber <77230787+maxcyber882@users.noreply.github.com>
2024-07-18 22:25:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = "Maxcyber";
+let index = 0;
+const speed = 150; // Speed of typing
+const cursor = '<span class="cursor">|</span>';
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById("output").innerHTML = '<span id="prompt">$ </span>' + text.substring(0, index + 1) + cursor;
+        index++;
+        setTimeout(typeWriter, speed);
+    } else {
+        document.getElementById("output").innerHTML = '<span id="prompt">$ </span>' + text; // Remove cursor after typing
+    }
+}
+
+window.onload = typeWriter;
